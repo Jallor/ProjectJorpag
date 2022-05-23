1dba05344e207a6c93e8c9eb4b4b4d3645764bf2
Jallor <romain-chevalier7@live.fr>
2022-05-24 01:54:45 +0200

    V1.01 of LevelEditorWindowEditor
    - allow to load TileGridMaps

--- Assets/Scripts/Editor/EditorWindow/LevelEditorWindowEditor.cs
+++ Assets/Scripts/Editor/EditorWindow/LevelEditorWindowEditor.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 using UnityEditor;
@@ -8,22 +9,19 @@ public class LevelEditorWindowEditor : EditorWindow
 {
     private const string _LevelEditorSceneName = "LevelEditor";
     private const string _TileMapsPath = "Datas/TileGridMaps";
-    private string _SelectedTileMapName = "NewTileMap";
+
     private static LevelEditorWindowEditor _CurrentWindow;
+
+    private string _SelectedTileMapName = "NewTileMap";
+    private int _SelectMapToOpenIndex = 0;
+    private string[] _TileGridMapNameList = new string[0];
 
     [MenuItem("Editors/Level Editor")]
     public static void ShowWindow()
     {
-        if (SceneManager.GetActiveScene().name == _LevelEditorSceneName)
-        {
-            _CurrentWindow = (LevelEditorWindowEditor)GetWindow(typeof(LevelEditorWindowEditor));
-            _CurrentWindow.Show();
-            _CurrentWindow.minSize = new Vector2(400, 400);
-        }
-        else
-        {
-            Debug.LogError("Can be open only in scene " + _LevelEditorSceneName);
-        }
+        _CurrentWindow = (LevelEditorWindowEditor)GetWindow(typeof(LevelEditorWindowEditor));
+        _CurrentWindow.Show();
+        _CurrentWindow.minSize = new Vector2(400, 400);
     }
 
     [MenuItem("Editors/Debug/Reset Level Editor", priority = 0)]
@@ -36,25 +34,84 @@ public class LevelEditorWindowEditor : EditorWindow
     {
         if (SceneManager.GetActiveScene().name != _LevelEditorSceneName)
         {
-            _CurrentWindow.Close();
+            GUILayout.Label("Wrong Scene ! Open " + _LevelEditorSceneName + " Scene.");
+            if (GUILayout.Button("Open " + _LevelEditorSceneName))
+            {
+                EditorSceneManager.OpenScene("Assets/Scenes/" + _LevelEditorSceneName + ".unity");
+            }
             return;
         }
 
         GUILayout.BeginVertical();
 
-        GUILayout.Label("TileGridMap name");
+        GUILayout.Label("TileGridMap name to save");
         _SelectedTileMapName = GUILayout.TextField(_SelectedTileMapName);
-
         if (GUILayout.Button("Save TileGridMap"))
         {
-            GameTileGrid tileGrid = FindObjectOfType<GameTileGrid>();
-            SerializedTileGridMapData tileGrideMapData = tileGrid.SerializeTileGridMap();
-
-            AssetDatabase.CreateAsset(tileGrideMapData, $"Assets/{_TileMapsPath}/{_SelectedTileMapName}.asset");
-            AssetDatabase.Refresh();
-            AllTileGridMapsDataList.Inst.RefreshDataList();
+            SaveMap();
         }
 
+        GUILayout.Space(5);
+
+        GUILayout.Label("TileGridMap to open");
+        _SelectMapToOpenIndex = EditorGUILayout.Popup(_SelectMapToOpenIndex, _TileGridMapNameList);
+        if (GUILayout.Button("Load TileGridMap"))
+        {
+            LoadMap();
+        }
+
+
         GUILayout.EndVertical();
+    }
+
+    private void SaveMap()
+    {
+        GameTileGrid tileGrid = FindObjectOfType<GameTileGrid>();
+        SerializedTileGridMapData tileGrideMapData = tileGrid.SerializeTileGridMap();
+
+        AssetDatabase.CreateAsset(tileGrideMapData, $"Assets/{_TileMapsPath}/{_SelectedTileMapName}.asset");
+        AssetDatabase.Refresh();
+        AllTileGridMapsDataList.Inst.RefreshDataList();
+
+        UpdateTileGridMapList();
+    }
+
+    private void LoadMap()
+    {
+        SerializedTileGridMapData tileGridMapData = AllTileGridMapsDataList.Inst.dataList[_SelectMapToOpenIndex];
+        GameTileGrid tileGrid = FindObjectOfType<GameTileGrid>();
+
+        tileGrid.LoadTileGridMap(tileGridMapData);
+    }
+
+    public void Awake()
+    {
+        HeavyGuiChange();
+    }
+
+    public void OnFocus()
+    {
+        HeavyGuiChange();
+        UpdateTileGridMapList();
+    }
+
+    public void OnInspectorUpdate()
+    {
+        HeavyGuiChange();
+    }
+
+    private void HeavyGuiChange()
+    {
+    }
+
+    private void UpdateTileGridMapList()
+    {
+        List<SerializedTileGridMapData> mapList = AllTileGridMapsDataList.Inst.dataList;
+
+        _TileGridMapNameList = new string[mapList.Count];
+        for (int i = 0; i < mapList.Count; i++)
+        {
+            _TileGridMapNameList[i] = mapList[i].name;
+        }
     }
 }
--- Assets/Scripts/Gameplay/GameTileGrid.cs
+++ Assets/Scripts/Gameplay/GameTileGrid.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System.Linq;
 
 using static SerializedTileGridMapData;
 
@@ -14,7 +15,7 @@ public class GameTileGrid : MonoBehaviour
     }
 
     [SerializeField] private List<Tilemap> _BackgroundTilemaps = new List<Tilemap>();
-    [SerializeField] private List<Tilemap> _Tilemaps = new List<Tilemap>();
+    [SerializeField] private List<Tilemap> _WallsTilemaps = new List<Tilemap>();
     [SerializeField] private List<Tilemap> _FrontgroundTilemaps = new List<Tilemap>();
 
     public void Start()
@@ -28,21 +29,21 @@ public class GameTileGrid : MonoBehaviour
 
         for (int i = 0; i < _BackgroundTilemaps.Count; i++)
         {
-            tileGrideMap._TileMapsLayers.Add(SerializeTilemap(_BackgroundTilemaps[i], TilemapTag.BACKGROUND, i));
+            tileGrideMap._TileMapsLayers.Add(SerializeTilemapLayer(_BackgroundTilemaps[i], TilemapTag.BACKGROUND, i));
         }
-        for (int i = 0; i < _Tilemaps.Count; i++)
+        for (int i = 0; i < _WallsTilemaps.Count; i++)
         {
-            tileGrideMap._TileMapsLayers.Add(SerializeTilemap(_Tilemaps[i], TilemapTag.WALL, i));
+            tileGrideMap._TileMapsLayers.Add(SerializeTilemapLayer(_WallsTilemaps[i], TilemapTag.WALL, i));
         }
         for (int i = 0; i < _FrontgroundTilemaps.Count; i++)
         {
-            tileGrideMap._TileMapsLayers.Add(SerializeTilemap(_FrontgroundTilemaps[i], TilemapTag.FRONTGROUND, i));
+            tileGrideMap._TileMapsLayers.Add(SerializeTilemapLayer(_FrontgroundTilemaps[i], TilemapTag.FRONTGROUND, i));
         }
 
         return (tileGrideMap);
     }
 
-    private SerializedTilemapLayer SerializeTilemap(Tilemap tilemap, TilemapTag tag, int index)
+    private SerializedTilemapLayer SerializeTilemapLayer(Tilemap tilemap, TilemapTag tag, int index)
     {
         SerializedTilemapLayer tilemapLayer = new SerializedTilemapLayer();
 
@@ -68,5 +69,54 @@ public class GameTileGrid : MonoBehaviour
         }
 
         return (tilemapLayer);
+    }
+
+    public void Clear()
+    {
+        List<Tilemap> tilemaps = new List<Tilemap>();
+        tilemaps.AddRange(_BackgroundTilemaps);
+        tilemaps.AddRange(_WallsTilemaps);
+        tilemaps.AddRange(_FrontgroundTilemaps);
+
+        foreach (Tilemap tilemap in tilemaps)
+        {
+            tilemap.ClearAllTiles();
+        }
+    }
+
+    public void LoadTileGridMap(SerializedTileGridMapData tileGridMap)
+    {
+        Clear();
+
+
+        foreach (SerializedTilemapLayer mapLayer in tileGridMap._TileMapsLayers)
+        {
+            Tilemap tilemapToUse = mapLayer.Tag switch
+            {
+                TilemapTag.BACKGROUND => _BackgroundTilemaps[mapLayer.Index],
+                TilemapTag.WALL => _WallsTilemaps[mapLayer.Index],
+                TilemapTag.FRONTGROUND => _FrontgroundTilemaps[mapLayer.Index]
+            };
+
+            TileBase[] tileList = new TileBase[mapLayer.TileList.Count];
+
+            for (int i = 0; i < mapLayer.TileList.Count; i++)
+            {
+                if (mapLayer.TileList[i].TileType != ETileType.NONE)
+                {
+                    TileBase tile;
+                    string tileNameToFind = mapLayer.TileList[i].TileType.ToString();
+                    tile = Instantiate(AllTilesList.Inst.dataList.Where(t => t.name == tileNameToFind).First());
+                    tileList[i] = tile;
+                }
+            }
+
+            BoundsInt bounds = new BoundsInt();
+            bounds.position = new Vector3Int((int)mapLayer.BoundsCenter.x,
+                (int)mapLayer.BoundsCenter.y, (int)mapLayer.BoundsCenter.z);
+            bounds.size = new Vector3Int((int)mapLayer.BoundsSize.x,
+                (int)mapLayer.BoundsSize.y, (int)mapLayer.BoundsSize.z);
+            tilemapToUse.SetTilesBlock(bounds, tileList);
+        }
     }
 }
