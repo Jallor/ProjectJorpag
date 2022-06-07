767bc043a50b5a26d2247991a5d6f1abd2c992b4
rchevalier <rchevalier@virtuosgames.com>
2022-06-07 14:11:35 +0200

    Add data for weapons

--- Assets/Scripts/ScriptableObjects/WeaponData.cs
+++ Assets/Scripts/ScriptableObjects/WeaponData.cs
@@ -3,4 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewWeaponData", menuName = "Data/Weapon Data")]
 public class WeaponData : ScriptableObject
 {
+    public Sprite Sprite = null;
+    [Tooltip("Rotation to fit with orientation DOWN")]
+    public float BaseRotation = 135f;
 }
