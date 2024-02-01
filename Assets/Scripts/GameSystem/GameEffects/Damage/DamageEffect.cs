1cef1a97df0e3d1122be9d6ceab558bcde28d52e
Romain CHEVALIER <rchevalier@virtuosgames.com>
2024-02-01 14:28:24 +0100

    [PROG] Start working on DamageEffect

---
+++ Assets/Scripts/GameSystem/GameEffects/Damage/DamageEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[SelectImplementationName("Damage/Damage Effect")]
+public class DamageEffect : GameEffect
+{
+    [SerializeField] float _DamageQuantity = 10;
+    [SerializeField] FirstGameVarSelector _DamageTarget
+        = new VarSelectorCharacter() { Target = VarSelectorCharacter.EPossibleTarget.TARGET };
+
+    public override bool PlayEffect(GameContext context)
+    {
+        GameVarWrapper targetWrapper = _DamageTarget.StartGetFinalVarWrapper(context);
+
+        if (targetWrapper.GetGameVarType() != EGameVarType.CHARACTER)
+        {
+            return false;
+        }
+
+        CharacterVarWrapper characterTarget = targetWrapper as CharacterVarWrapper;
+        characterTarget.Character.
+
+        return true;
+    }
+}
