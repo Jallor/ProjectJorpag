16d0c84ad24e94fb8df8a8179033788a7825c9fe
Romain CHEVALIER <rchevalier@virtuosgames.com>
2024-02-07 14:15:37 +0100

    [PROG] Start implement weapon object and possiblity to have multiple weapons

---
+++ Assets/Scripts/Gameplay/WeaponObject.cs
@@ -0,0 +1,13 @@
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponObject : MonoBehaviour
+{
+    [Required] [SerializeField] private TriggerArea _SkillTriggerArea = null;
+
+    
+
+
+}
--- Assets/Scripts/ScriptableObjects/WeaponData.cs
+++ Assets/Scripts/ScriptableObjects/WeaponData.cs
@@ -4,8 +4,10 @@ using NaughtyAttributes;
 [CreateAssetMenu(fileName = "NewWeaponData", menuName = "Data/Weapon")]
 public class WeaponData : ScriptableObject
 {
+    public string WeaponName;
     [ShowAssetPreview]
     public Sprite Sprite = null;
+    public GameObject WeaponObject = null;
     [Tooltip("Rotation to fit with orientation DOWN")]
     public float BaseRotation = 135f;
 }
