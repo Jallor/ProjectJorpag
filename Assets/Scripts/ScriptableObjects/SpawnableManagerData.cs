bb731d3f939db04a891d5bfacf110afb0c181e37
Romain CHEVALIER <rchevalier@virtuosgames.com>
2024-10-09 14:14:07 +0200

    [PROG] Add SpawnableManagerData
    (need to find what to had in this)

---
+++ Assets/Scripts/ScriptableObjects/SpawnableManagerData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSpawnableManagerData", menuName = "Data/SpawnableManager")]
+public class SpawnableManagerData : ScriptableObject, IWorldEntityData
+{
+}
